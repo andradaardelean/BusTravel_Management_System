3d5f2636e42cdda86f42733a955b03837f9772c3
Andrada Ardelean <120036981+andradaardelean@users.noreply.github.com>
2024-03-30 12:26:18 +0200

    cors

--- bustravel/bustravel/src/main/java/com/licenta/bustravel/controller/UserController.java
+++ bustravel/bustravel/src/main/java/com/licenta/bustravel/controller/UserController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 public class UserController {
     @Autowired
     private UserService userService;
